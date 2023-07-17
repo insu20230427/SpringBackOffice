b76d2320043f6e08af611cf2e83375983bde573c
BenKim95 <forseminaar@gmail.com>
2023-07-17 14:28:23 +0900

    init commit

---
+++ src/test/java/com/example/springbackoffice/SpringBackOfficeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootTest
+class SpringBackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
