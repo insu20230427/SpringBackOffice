83bbdcae967bf0585d27f8c97d06e26900991824
insu <insubono@gmail.com>
2023-07-17 17:18:56 +0900

    <Dto> PostResponseDto, PostRequestDto init commit

---
+++ src/main/java/com/example/springbackoffice/dto/PostRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.springbackoffice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+    private String title;
+    private String content;
+}
+
---
+++ src/main/java/com/example/springbackoffice/dto/PostResponseDto.java
@@ -0,0 +1,36 @@
+package com.example.springbackoffice.dto;
+
+import com.example.springbackoffice.entity.Post;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long user_id;
+    private String userName;
+    private String userNickName;
+    private List<CommentResponseDto> commentResponseDtoList;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
+        this.user_id = post.getUser().getId();
+        this.userName = post.getUser().getName();
+        this.userNickName = post.getUser().getNickname();
+        if(post.getCommentList().size()>0) {
+            this.commentResponseDtoList = new ArrayList<>();
+            for (Comment comment : post.getCommentList()) {
+                this.commentResponseDtoList.add(new CommentResponseDto(comment));
+            }
+        }
+    }
+}
