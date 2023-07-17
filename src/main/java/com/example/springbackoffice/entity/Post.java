f8b2e7988ac2093fc40ad8cf484b88004506d59a
insu <insubono@gmail.com>
2023-07-17 19:49:59 +0900

    <Entity> Post, Timestamped init Commit

---
+++ src/main/java/com/example/springbackoffice/entity/Post.java
@@ -0,0 +1,53 @@
+package com.example.springbackoffice.entity;
+
+import com.example.springbackoffice.dto.PostRequestDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import com.example.springbackoffice.entity.User;
+import java.util.ArrayList;
+@Entity
+@Getter
+@Setter
+@Table(name = "post")
+@NoArgsConstructor
+public class Post extends Timestamped { // 상속받아서 createdAt, modifiedAt column 가져옴
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 3000)
+    private String content;
+
+    @Column(nullable = false)
+    private int post_like_count;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+
+    @OneToMany( mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> commentList = new ArrayList<>();
+
+    public Post(PostRequestDto requestDto, User user) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.user = user;
+    }
+
+    public void addComment(Comment comment) {
+        this.commentList.add(comment);
+    }
+
+    public void updatePost(PostRequestDto postRequestDto) {
+        this.title = postRequestDto.getTitle();
+        this.content = postRequestDto.getContent();
+    }
+}
