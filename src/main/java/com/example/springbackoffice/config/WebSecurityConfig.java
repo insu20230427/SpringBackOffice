3945f4f547b70000e742b8177880628c927c37df
insu <insubono@gmail.com>
2023-07-19 19:00:18 +0900

    <congig> WebSecurityConfig 의 접근 권한 설정 수정 및 주석 처리

--- src/main/java/com/example/springbackoffice/config/WebSecurityConfig.java
+++ src/main/java/com/example/springbackoffice/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -64,8 +65,9 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers(HttpMethod.GET,"/api/posts/**").permitAll() // 게시글 조회는 인증없이도 가능하기 때문에 허가해준다. -> posts 의 get 요청들 2개 빼고 인가받게 설정
+//                        .requestMatchers(HttpMethod.GET,"/api/posts/**").permitAll() // 게시글 조회는 인증없이도 가능하기 때문에 허가해준다. -> posts 의 get 요청들 2개 빼고 인가받게 설정
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/post/**").permitAll()
                         .requestMatchers("/api/users/**").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
 
