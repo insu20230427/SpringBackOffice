040d0897bf7b8461b2260eb6a7c82a74624a461e
insu <insubono@gmail.com>
2023-07-19 19:02:46 +0900

    <controller> UserController 중 31,33,41,47번째 줄 인자 순서 변경

--- src/main/java/com/example/springbackoffice/controller/UserController.java
+++ src/main/java/com/example/springbackoffice/controller/UserController.java
@@ -28,9 +28,9 @@ public class UserController {
 //            requestDto.setAuthKey(authKey);
             userService.signup(requestDto); //회원 가입을 처리하기 위해 userService.signup(requestDto)를 호출
         } catch (IllegalArgumentException e) { // 중복된 username 이 있는 경우
-            return ResponseEntity.badRequest().body(new ApiResponseDto("이미 존재하는 id 입니다. 다른 id를 입력해 주세요", HttpStatus.BAD_REQUEST.value()));
+            return ResponseEntity.badRequest().body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 id 입니다. 다른 id를 입력해 주세요"));
         }
-        return ResponseEntity.status(201).body(new ApiResponseDto("회원가입 완료 되었습니다.", HttpStatus.CREATED.value()));
+        return ResponseEntity.status(201).body(new ApiResponseDto(HttpStatus.CREATED.value(), "회원가입 완료 되었습니다."));
     }
     //로그인
     @PostMapping("/auth/login")
@@ -38,13 +38,13 @@ public class UserController {
         try {
             userService.login(loginRequestDto);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(new ApiResponseDto(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST.value()));
+            return ResponseEntity.badRequest().body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), e.getLocalizedMessage()));
         }
 
         // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(loginRequestDto.getUsername(), loginRequestDto.getRole()));
 
-        return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.OK.value()));
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "로그인 성공"));
     }
 
 }
