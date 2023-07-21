bd42bb45ba7061a545d909a6b717dce060e2d4e4
dilution0216 <hglad0216@naver.com>
2023-07-21 17:29:08 +0900

    (Repository) UserRepository 에 findBykakaoId, findByEmail 메서드 구현

--- src/main/java/com/example/springbackoffice/repository/UserRepository.java
+++ src/main/java/com/example/springbackoffice/repository/UserRepository.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    //인증이 된 유저만 이름으로 조회
-    // Optional<User> findByEmail(String email);
-}
 
+    Optional<User> findByKakaoId(Long kakaoId);
+    //인증이 된 유저만 이름으로 조회
+     Optional<User> findByEmail(String email);
+}
